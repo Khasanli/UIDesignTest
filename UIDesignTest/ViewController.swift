32523f2d9598fcde95146e60346291676d2a40cb
samirhasanli2 <khasanli2021@gmail.com>
2021-09-26 19:18:50 +0400

    Initial Commit

---
+++ UIDesignTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIDesignTest
+//
+//  Created by Samir Hasanli on 26.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
